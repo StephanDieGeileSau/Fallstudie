f5e5f7731e9fc74638167c998ef1f2f9604282c2
Lenert <stephan.lenert@hpe.com>
2020-02-10 21:38:22 +0100

    fixes backlink and action buttons

--- src/components/Dashboard_elements/ActionPages/Überweisung.jsx
+++ src/components/Dashboard_elements/ActionPages/Überweisung.jsx
@@ -1,50 +1,55 @@
 import React from 'react'
 
 export default function Überweisung() {
-    return (
+    return(
         <div>
-            <div>
-                <a href="fts_konto.htm">
-                    <h1 class="ntc">NTC</h1>
-                </a>
-            </div>
-            <div>
-                <h5 class="ntc2">Banking </h5>
-            </div>
-
-            <table>
-                <form action="beratungsgespraech.php" method="post">
-                    <tr>
-                        <th colspan="2">
-                            <h3>Überweisungsauftrag abgeben:</h3>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th class="formular">Zahlungsempfänger:</th>
-                        <td class="daten"><input placeholder="Name, Vorname" class="daten" type="text" name="empfaenger"/></td>
-                    </tr>
-                    <tr>
-                        <th class="formular">IBAN:</th>
-                        <td><input placeholder="DE00 0000 0000 0000 0000 00" class="daten" type="text" name="iban"/></td>
-                    </tr>
-                    <tr>
-                        <th class="formular">BIC:</th>
-                        <td class="daten"><input placeholder="Bei Eingabe von IBAN nicht erforderlich." class="daten" type="text" name="bic"/></td>
-                    </tr>
-                    <tr>
-                        <th class="formular">Betrag:</th>
-                        <td class="daten"><input placeholder="Euro, Cent" class="daten" type="text" name="betrag"/></td>
-                    </tr>
-                    <tr>
-                        <th class="formular">Verwendungszweck:</th>
-                        <td class="daten"><input placeholder="Maximal 54 Stellen" class="daten" type="text" name="verwendungszweck"/></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><br></br><input class="button" type="Submit" value="Abschicken"/></td>
-                    </tr>
-                </form>
-            </table>
-            
+            Überweisung
         </div>
     )
+    // return (
+    //     <div>
+    //         <div>
+    //             <a href="fts_konto.htm">
+    //                 <h1 class="ntc">NTC</h1>
+    //             </a>
+    //         </div>
+    //         <div>
+    //             <h5 class="ntc2">Banking </h5>
+    //         </div>
+
+    //         <table>
+    //             <form action="beratungsgespraech.php" method="post">
+    //                 <tr>
+    //                     <th colspan="2">
+    //                         <h3>Überweisungsauftrag abgeben:</h3>
+    //                     </th>
+    //                 </tr>
+    //                 <tr>
+    //                     <th class="formular">Zahlungsempfänger:</th>
+    //                     <td class="daten"><input placeholder="Name, Vorname" class="daten" type="text" name="empfaenger"/></td>
+    //                 </tr>
+    //                 <tr>
+    //                     <th class="formular">IBAN:</th>
+    //                     <td><input placeholder="DE00 0000 0000 0000 0000 00" class="daten" type="text" name="iban"/></td>
+    //                 </tr>
+    //                 <tr>
+    //                     <th class="formular">BIC:</th>
+    //                     <td class="daten"><input placeholder="Bei Eingabe von IBAN nicht erforderlich." class="daten" type="text" name="bic"/></td>
+    //                 </tr>
+    //                 <tr>
+    //                     <th class="formular">Betrag:</th>
+    //                     <td class="daten"><input placeholder="Euro, Cent" class="daten" type="text" name="betrag"/></td>
+    //                 </tr>
+    //                 <tr>
+    //                     <th class="formular">Verwendungszweck:</th>
+    //                     <td class="daten"><input placeholder="Maximal 54 Stellen" class="daten" type="text" name="verwendungszweck"/></td>
+    //                 </tr>
+    //                 <tr>
+    //                     <td colspan="2"><br></br><input class="button" type="Submit" value="Abschicken"/></td>
+    //                 </tr>
+    //             </form>
+    //         </table>
+            
+    //     </div>
+    // )
 }
