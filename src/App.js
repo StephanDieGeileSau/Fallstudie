25f7487c973ae64ef8ce3140dc7aa5843c5cb7c4
Lenert <stephan.lenert@hpe.com>
2020-02-11 11:25:35 +0100

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import Home from "./components/Home.js"
 import ReactGA from 'react-ga';
-import auth from './auth.ts';
+//import auth from './auth.ts';
 
 export default class App extends Component {
   render() {
 
     const trackingId = "UA-156025636-1"; 
     ReactGA.initialize(trackingId);
-    ReactGA.set({userId: auth.currentUserId()})
+    //ReactGA.set({userId: auth.currentUserId()})
 
     return (
       <div>
